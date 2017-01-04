b4dc2fcade042bb5e7fa876c0c03900391dd3c4a
Jonas Gierer <jgierer12@outlook.com>
2017-01-04 17:40:10 +0100

    Finish exercise 6

--- 01-intro/towers-of-hanoi.hs
+++ 01-intro/towers-of-hanoi.hs
@@ -1,7 +1,14 @@
 type Peg = String
 type Move = (Peg, Peg)
 
+-- Get a list of steps to solve the [Tower Of Hanoi](https://en.wikipedia.org/wiki/Tower_of_Hanoi) puzzle
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n from to temp
   | n <= 0 = []
   | otherwise = (hanoi (n-1) from temp to) ++ [(from, to)] ++ (hanoi (n-1) temp to from)
+
+-- Like `hamoi`, but with four pegs instead
+hochiminh :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hochiminh n from to temp1 temp2
+  | n <= 0 = []
+  | otherwise = (hochiminh (n-2) from temp1 temp2 to) ++ [(from, temp2), (from, to), (temp2, to)] ++ (hochiminh (n-2) temp1 to from temp2)
